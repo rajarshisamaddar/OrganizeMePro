e42b82d9350882853e14d72633d911fb087c4285
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-01-25 20:37:32 +0530

    feat: category routes

---
+++ src/routes/category.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+// TODO: post => create new category
+// TODO: get => get all categories
+// TODO: patch => update category details
+// TODO: delete => delete category
+// TODO: get => get all tasks in a category
+
+export default router;
