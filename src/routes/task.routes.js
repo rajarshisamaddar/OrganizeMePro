36e48bd044ba3ef8b91144ad5dc612c670050b79
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-01-25 20:37:39 +0530

    feat: task routes

---
+++ src/routes/task.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+// TODO: post => create new task
+// TODO: get => get all tasks
+// TODO: patch => update task details
+// TODO: delete => delete task
+
+export default router;
