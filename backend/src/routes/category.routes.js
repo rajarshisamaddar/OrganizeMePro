d54e2f6eb84817ca6bedf6b333c5da8e90675ba3
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-01-26 18:48:13 +0530

    feat: category controller

--- backend/src/routes/category.routes.js
+++ backend/src/routes/category.routes.js
@@ -1,13 +1,27 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import {
+  createCategory,
+  getAllCategories,
+  addCollaborator,
+  updateCategory,
+} from "../controllers/category.controller.js";
 
 const router = Router();
 
-// TODO: post => create new category
-// TODO: get => get all categories
-// TODO: patch => update category details
-// TODO: get => get all tasks in a category
+// COMPLETE: post => create new category
+router.route("/create").post(verifyJWT, createCategory);
 
+// COMPLETE: get => get all categories
+router.route("/get-all-categories").get(verifyJWT, getAllCategories);
+
+// COMPLETE: post => add collaborator to category
+router.route("/add-collaborator").post(verifyJWT, addCollaborator);
+
+// TODO: patch => update category details by owner only
+router.route("/update/:categoryId").patch(verifyJWT, updateCategory);
+
+// TODO: get => get all tasks in a category
 // TODO: delete => delete category
 
 export default router;
