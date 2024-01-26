cf6268640bbba62412d87c6ed087dbc30ffd35fa
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-01-26 16:20:44 +0530

    feat: style desc category model

--- backend/src/models/category.model.js
+++ backend/src/models/category.model.js
@@ -2,15 +2,24 @@ import mongoose, { Schema } from "mongoose";
 
 const CategorySchema = new Schema(
   {
-    name: {
+    title: {
       type: String,
       required: true,
       trim: true,
       maxlength: [128, "Category name can be 128 characters long at most!"],
       unique: [true, "Category with this name already exists"],
     },
+    description: {
+      type: String,
+      trim: true,
+      maxlength: [256, "Description can be 256 characters long at most!"],
+    },
+    style: {
+      type: Object,
+    },
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
     todo: [{ type: Schema.Types.ObjectId, ref: "Todo" }],
+    collaborators: [{ type: Schema.Types.ObjectId, ref: "User" }],
   },
   { timestamps: true }
 );
