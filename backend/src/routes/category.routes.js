ccd19a92ffa3944a4b9784228ab34078563b648b
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-01-26 16:21:36 +0530

    fix: router

--- backend/src/routes/category.routes.js
+++ backend/src/routes/category.routes.js
@@ -6,7 +6,8 @@ const router = Router();
 // TODO: post => create new category
 // TODO: get => get all categories
 // TODO: patch => update category details
-// TODO: delete => delete category
 // TODO: get => get all tasks in a category
+
+// TODO: delete => delete category
 
 export default router;
