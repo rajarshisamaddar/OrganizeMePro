6e9758ac379bef470fc1c5ae96ee318bd7a442f4
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-01-27 13:41:28 +0530

    feat: home sample fetch add

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axiosCustom from "../utils/axiosCustom";
 
 const Home = () => {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axiosCustom.get("/category/get-all-categories");
+      setData(result.data.categories[0]);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
-      
+      <p>{data.title}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
