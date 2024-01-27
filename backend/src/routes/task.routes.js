1849b28c8262ad756ce8620d09505c6148c292f0
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-01-27 15:48:11 +0530

    feat: task routes

--- backend/src/routes/task.routes.js
+++ backend/src/routes/task.routes.js
@@ -1,9 +1,22 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import {
+  createTask,
+  getAllTasks,
+  getTasksByCategory,
+} from "../controllers/task.controller.js";
 
 const router = Router();
 
-// TODO: post => create new task
+// ALPHA: post => create new task
+router.route("/create").post(verifyJWT, createTask);
+
+// ALPHA: get => get all tasks of a user and tasks of a category if user is a collaborator of that category
+router.route("/get-all").get(verifyJWT, getAllTasks);
+
+// ALPHA: get => get all tasks in a category
+router.route("/get-by-category/:categoryId").get(verifyJWT, getTasksByCategory);
+
 // TODO: patch => update a particular task
 // TODO: delete => delete a particular task
 
