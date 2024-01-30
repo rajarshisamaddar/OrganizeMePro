9792ce51dfd4aac3bdc6ce6e610da938c7047677
soumayadip2002 <sinuroy696@gmail.com>
2024-01-30 22:12:56 +0530

    refactor: updated ui

--- frontend/src/pages/Category.jsx
+++ frontend/src/pages/Category.jsx
@@ -7,15 +7,15 @@ import TasksLayout from "@/components/Layouts/TasksLayout";
 import { TbPlaylistAdd } from "react-icons/tb";
 import GridView from "@/components/shared/GridView";
 import ListView from "@/components/shared/ListView";
+import Loading from "@/components/Loading/Loading";
 const Category = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [currentStatus, setCurrentStatus] = useState("pending");
-  const [gridView, setGridView] = useState(true);
   const { categories } = useSelector((state) => state.category);
   const category = categories && categories.find((item) => item._id === id);
-  const { allTasks } = useSelector((state) => state.tasks);
+  const { allTasks, layout } = useSelector((state) => state.tasks);
   useEffect(() => {
     const getTasks = async () => {
       const categoryWiseTask = await getTasksByCategory(id);
@@ -25,13 +25,13 @@ const Category = () => {
     };
     getTasks();
   }, [id]);
+  if (!category || !allTasks) return <Loading />;
   return (
     <TasksLayout
-      name={category && category.title}
+      category={category}
       setCurrentStatus={setCurrentStatus}
       currentStatus={currentStatus}
-      gridView={gridView}
-      setGridView={setGridView}
+      hasMember={category.collaborators.length > 0 && true}
     >
       <div>
         <div className="w-full px-8 flex gap-3 justify-end sm:justify-center items-center">
@@ -39,13 +39,13 @@ const Category = () => {
             className="flex items-center gap-1 outline-dashed bg-transparent outline-indigo-600 p-2 px-4
                 justify-center outline-[1.5px] rounded-md text-indigo-600 text-sm font-semibold 
                 transition-transition hover:bg-indigo-600 hover:outline hover:text-white"
-                onClick={()=>navigate(`/addTask/${id}`)}
+            onClick={() => navigate(`/addTask/${id}`)}
           >
             <TbPlaylistAdd className="text-xl" />
             Add Task
           </button>
         </div>
-        {gridView ? (
+        {layout === "grid" ? (
           <GridView allTasks={allTasks} currentStatus={currentStatus} />
         ) : (
           <ListView allTasks={allTasks} currentStatus={currentStatus} />
