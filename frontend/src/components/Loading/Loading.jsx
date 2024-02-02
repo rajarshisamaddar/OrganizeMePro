5b0ca4eea7684c25fd40d8b1620ee864a26cfc66
soumayadip2002 <sinuroy696@gmail.com>
2024-02-02 13:29:38 +0530

    updated loading ui🚀

--- frontend/src/components/Loading/Loading.jsx
+++ frontend/src/components/Loading/Loading.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
+import LoadingImage from '@/assets/loading.svg'
 const Loading = () => {
   return (
-    <div className='h-screen bg-black flex justify-center items-center text-5xl'>
-      Loading....
+    <div className="h-screen w-full bg-cardBg border border-border flex justify-center items-center">
+      <img src={LoadingImage} alt="" className="h-14 w-14" />
+      <h2 className="mt-2 text-lg font-semibold">loading...</h2>
     </div>
-  )
-}
+  );
+};
 
-export default Loading
+export default Loading;
