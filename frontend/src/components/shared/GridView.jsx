308244b7d0393adc4362268c0f9019c3968699fc
soumayadip2002 <sinuroy696@gmail.com>
2024-02-03 11:33:23 +0530

    added assign option

--- frontend/src/components/shared/GridView.jsx
+++ frontend/src/components/shared/GridView.jsx
@@ -7,9 +7,12 @@ import { MdDelete } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 import { updateTask } from "@/utils/tasksService";
 import toast from "react-hot-toast";
-const GridView = ({ allTasks, currentStatus }) => {
+import { LuPlusCircle } from "react-icons/lu";
+import { getAcronym } from "@/data/getAcronym";
+const GridView = ({ allTasks, currentStatus, hasMember, category }) => {
   const dispatch = useDispatch();
   const [status, setStatus] = useState({});
+  const [openAssign, setOpenAssign] = useState({});
   const handleTaskStatus = async (taskId, newStatus) => {
     setStatus((prev) => ({
       ...prev,
@@ -25,6 +28,25 @@ const GridView = ({ allTasks, currentStatus }) => {
       toast.success(`task is ${newStatus}`);
     }
   };
+
+  const handleTaskAssign = (taskId) => {
+    setOpenAssign((prev) => ({
+      ...prev,
+      [taskId]: !prev[taskId],
+    }));
+  };
+
+  const handleAssign = async (taskId, member) => {
+    const assigned = await updateTask({
+      id: taskId,
+      data: { assignedTo: member },
+    });
+    if (assigned) {
+      dispatch(setUpdatedTask(assigned));
+      toast.success(`Assigned to ${member}`);
+      handleTaskAssign(taskId);
+    }
+  };
   const navigate = useNavigate();
   return (
     <div className="grid grid-cols-3 gap-4 md:grid-cols-2 sm:grid-cols-1 mt-4 px-8 sm:px-3">
@@ -34,7 +56,7 @@ const GridView = ({ allTasks, currentStatus }) => {
             task.status === currentStatus && (
               <div
                 key={task._id}
-                className="h-[18rem] flex flex-col justify-between w-auto cursor-pointer 
+                className="h-[18rem] flex flex-col gap-2 w-auto cursor-pointer relative 
                 bg-cardBg border border-border p-3 rounded-md overflow-hidden"
               >
                 <div className="flex w-full justify-between items-center">
@@ -52,6 +74,37 @@ const GridView = ({ allTasks, currentStatus }) => {
                     )}
                   </select>
                   <div className="flex items-center gap-3 text-xl">
+                    {hasMember && category && (
+                      <div>
+                        <div
+                          className="text-indigo-600 text-2xl"
+                          onClick={() => handleTaskAssign(task._id)}
+                        >
+                          <LuPlusCircle />
+                        </div>
+                        {openAssign[task._id] && (
+                          <div
+                            className="absolute h-[10rem] w-[10rem] custom-scroll overflow-y-auto top-12 p-2 bg-cardBg border border-border 
+                                              z-[1000] right-14  rounded-md shadow-md shadow-indigo-500/20"
+                          >
+                            <p className="text-sm font-semibold text-center mb-2">
+                              Assign to
+                            </p>
+                            {category.collaborators.length > 0 &&
+                              category.collaborators.map((item) => (
+                                <div
+                                  key={item}
+                                  onClick={() => handleAssign(task._id, item)}
+                                  className="text-sm my-1 bg-background p-1 rounded-sm hover:bg-primaryColor transition-transition hover:text-cardBg"
+                                >
+                                  {item.slice(0, 8)}
+                                </div>
+                              ))}
+                          </div>
+                        )}
+                      </div>
+                    )}
+
                     <div
                       className="text-green-600"
                       onClick={() => navigate(`/editTask/${task._id}`)}
@@ -65,7 +118,7 @@ const GridView = ({ allTasks, currentStatus }) => {
                 </div>
                 <div
                   onClick={() => navigate(`/viewTask/${task._id}`)}
-                  className="mt-3 overflow-hidden border border-border flex-1 bg-background rounded-md"
+                  className="mt-3 overflow-hidden border border-border bg-background rounded-md h-[65%]"
                 >
                   <div className="p-2  border-b border-border">
                     <p className="capitalize font-semibold text-sm">
@@ -80,6 +133,14 @@ const GridView = ({ allTasks, currentStatus }) => {
                           : task.description
                       }
                     />
+                  </div>
+
+                  <div className="absolute bottom-0 left-0 right-0 border-t border-border p-2 px-6 text-sm font-semibold">
+                    {
+                      <div className="flex items-center">
+                        <p>Assigned to - {task.assignedTo.slice(0, 10)}</p>
+                      </div>
+                    }
                   </div>
                 </div>
               </div>
